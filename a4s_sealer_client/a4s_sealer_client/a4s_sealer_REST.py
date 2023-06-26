0b907f064ce06ea3a22e4cc9cd55e2c8cbb11b12
tginsbu1 <tsg44@cornell.edu>
2023-06-26 17:58:18 -0500

    why won't async work

--- a4s_sealer_client/a4s_sealer_client/a4s_sealer_REST.py
+++ a4s_sealer_client/a4s_sealer_client/a4s_sealer_REST.py
@@ -1,23 +1,19 @@
 """The server that takes incoming WEI flow requests from the experiment application"""
-"""The server that takes incoming WEI flow requests from the experiment application"""
 import json
 from argparse import ArgumentParser
 from contextlib import asynccontextmanager
-
+import time
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import JSONResponse
-from a4s_sealer_driver.a4s_sealer_driver import A4S_SEALER_DRIVER  # import sealer driver
-import time
 
-# TODO: db backup of tasks and results (can be a proper db or just a file)
-# TODO logging for server and workcell
-# TODO consider sub-applications for different parts of the server (e.g. /job, /queue, /data, etc.)
-# TODO make the workcell live in the DATA_DIR and be coupled to the server
-#      This might entail making a rq object of the wei object and making that available to the workers
+from a4s_sealer_driver.a4s_sealer_driver import A4S_SEALER_DRIVER  # import sealer driver
 
 workcell = None
 global sealer
-Port = '/dev/ttyUSB1'
+serial_port = '/dev/ttyUSB0'
+local_ip = 'parker.alcf.anl.gov'
+local_port = '8000'
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -32,7 +28,7 @@ async def lifespan(app: FastAPI):
         -------
         None"""
     try:
-            sealer = A4S_SEALER_DRIVER(Port)
+            sealer = A4S_SEALER_DRIVER(serial_port)
     except Exception as err:
             state = "SEALER CONNECTION ERROR"
 
@@ -45,73 +41,51 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, )
 
-
-@app.post("/action")
-async def do_action(
-    action_handle: str,
-    action_vars: dict, 
-):
-    global sealer
-    if action_handle == 'seal':  
-        #self.sealer.set_time(3)
-        #self.sealer.set_temp(175)
-        try: 
-            time = vars.get('time',3)
-            temp = vars.get('temp',175)
-
-            sealer.seal()
-            time.sleep(15)  
-            response_content = {
-                    "action_msg": "StepStatus.Succeeded",
-                    "action_response": "True",
-                    "action_log": ""
-                    
-                }
-            return JSONResponse(content=response_content)
-        except Exception as e:
-            response_content = {
-            "status": "failed",
-            "error": str(e),
-        }
-            return JSONResponse(content=response_content)
-@app.post("/action")
-async def do_action(
-    action_handle: str,
-    action_vars: dict, 
-):
-    global sealer
-    if action_handle == 'seal':  
-        #self.sealer.set_time(3)
-        #self.sealer.set_temp(175)
-        try: 
-            time = vars.get('time',3)
-            temp = vars.get('temp',175)
-
-            sealer.seal()
-            time.sleep(15)  
-            response_content = {
-                    "action_msg": "StepStatus.Succeeded",
-                    "action_response": "True",
-                    "action_log": ""
-                    
-                }
-            return JSONResponse(content=response_content)
-        except Exception as e:
-            response_content = {
-            "status": "failed",
-            "error": str(e),
-        }
-            return JSONResponse(content=response_content)
-
-@app.post("/state")
-async def state(
-    action_handle: str,
-    action_vars: dict, 
-):
+@app.get("/state")
+async def state():
     global sealer
     return JSONResponse(content={"State": sealer.get_status() })
+
+@app.get("/description")
+async def description():
+    global sealer
+    return JSONResponse(content={"State": sealer.get_status() })
+
+@app.get("/resources")
+async def resources():
+    global sealer
+    return JSONResponse(content={"State": sealer.get_status() })
+
+
+@app.post("/action")
+async def do_action(
+    action_handle: str,
+    action_vars: dict, 
+):
+    global sealer
+    if action_handle == 'seal':  
+        #self.sealer.set_time(3)
+        #self.sealer.set_temp(175)
+        try: 
+          
+            sealer.seal()
+            time.sleep(15)  
+            response_content = {
+                    "action_msg": "StepStatus.Succeeded",
+                    "action_response": "True",
+                    "action_log": ""
+                    
+                }
+            return JSONResponse(content=response_content)
+        except Exception as e:
+            response_content = {
+            "status": "failed",
+            "error": str(e),
+        }
+            return JSONResponse(content=response_content)
+
 
 if __name__ == "__main__":
     import uvicorn
     print("asdfsaf")
-    uvicorn.run("rpl_wei.server:app", reload=True, ws_max_size=100000000000000000000000000000000000000)
+    uvicorn.run("a4s_sealer_REST:app", host=local_ip, port=local_port, reload=True, ws_max_size=100000000000000000000000000000000000000)
